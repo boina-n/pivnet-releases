6348b2871cd0e4fe083536416360eebd7a0d4080
Nadjmou BOINA <nadjmou.boina@gmail.com>
2017-10-13 23:14:09 +0200

    Update pivnet-releases.sh

--- pivnet-releases.sh
+++ pivnet-releases.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 source ./params.env
 pivnet login --api-token $api_token
-om --target $om_target --skip-ssl-validation --username $username --password $password staged-products | grep -e "^| [a-Z]" | awk '{ print $2 }' | while read p; do pivnet releases -p $p --format=json |  jq --arg p $p --tab '{ ($p): .[0] }' > releases/$p.release; done 
+om --target $om_target --skip-ssl-validation --username $username --password $password staged-products | tee releases/staged-products.current
+
+om --target $om_target --skip-ssl-validation --username $username --password $password staged-products | grep -e "^| [a-Z]" | awk '{ print $2 }' | while read p; do pivnet releases -p $p --format=json |  jq --arg p $p --tab '{ ($p): .[0] }' | tee  releases/$p.release; done 
+
+p=stemcells
+bosh -e pcf stemcells --json |jq --arg p $p --tab  '{ ($p) :.Tables[0].Rows[0]}' | tee releases/$p.current
+pivnet releases -p $p --format=json |  jq --arg p $p --tab '{ ($p): .[0] }' | tee releases/$p.release
